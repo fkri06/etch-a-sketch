fe06f801947b6c30efbdef309f7858fc25f961ba
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-14 06:23:19 +0700

    Add 16 x 16 divs inside container

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const container = document.querySelector("div.container");
+const container = document.querySelector(".container");
+
+// 16 x 16 grid square div inside container
+for (let i = 0; i < 16 * 16; i++) {
+    const childContainer = document.createElement("div");
+    childContainer.classList.add("child-container");
+    container.appendChild(childContainer);
+}
+
