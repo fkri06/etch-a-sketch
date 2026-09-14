2bddec688b1d037a8ea6400ab706829cf41e00ec
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-14 07:41:28 +0700

    Finish the logic
    - Refactor grid box sketch and hover effect
      by creating function for each actions.
    - Add remove grid box function to remove
      grid boxes after user entering a new
      box number.
    - Add event listener to the button.

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+let boxNumber;
 
-// 16 x 16 grid square div inside container
-for (let i = 0; i < 16 * 16; i++) {
-    const childContainer = document.createElement("div");
-    childContainer.classList.add("child-container");
-    container.appendChild(childContainer);
+function sketchGrid(n) {
+    // 16 x 16 grid square div inside container
+    for (let i = 0; i < n * n; i++) {
+        const childContainer = document.createElement("div");
+        childContainer.classList.add("child-container");
+        container.appendChild(childContainer);
+    }
 }
 
-const childContainers = document.querySelectorAll(".child-container");
-childContainers.forEach(child => {
-    child.addEventListener("mouseenter", () => {
-        child.style.backgroundColor = "red";
+function removeGrid() {
+    const childContainers = document.querySelectorAll(".child-container");
+    if (childContainers) {
+        childContainers.forEach((item) => item.remove());
+    }
+}
+
+function hover() {
+    const childContainers = document.querySelectorAll(".child-container");
+    childContainers.forEach(child => {
+        child.addEventListener("mouseenter", () => {
+            child.style.backgroundColor = "red";
+        });
     });
+}
+
+button.addEventListener("click", () => {
+    removeGrid();
+    boxNumber = Number.parseInt(prompt("Enter box number"));
+    sketchGrid(boxNumber);
+    hover();
 });
+
